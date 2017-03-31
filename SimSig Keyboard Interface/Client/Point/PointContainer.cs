9de6ec9769d65dce9a695e437c204e18fe5abda9
James Pedrick <james2.pedrick@outlook.com>
2017-04-01 00:37:36 +0100

    -

---
+++ SimSig Keyboard Interface/Client/Point/PointContainer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimSig_Keyboard_Interface
+{
+	public class PointContainer
+	{
+		public List<Point> PointList = new List<Point>;
+
+	}
+}
