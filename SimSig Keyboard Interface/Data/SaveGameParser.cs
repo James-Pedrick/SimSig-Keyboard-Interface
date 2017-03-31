cb74eaf6be1b2601c4636d941c3164e0672a76e9
James Pedrick <james2.pedrick@outlook.com>
2017-03-31 23:59:16 +0100

    Point data when loaded from a save game is now parsed. No variables assigned anywhere

---
+++ SimSig Keyboard Interface/Data/SaveGameParser.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimSig_Keyboard_Interface.Properties;
+using System.IO;
+
+
+
+namespace SimSig_Keyboard_Interface.Data
+{
+	class SaveGameParser
+	{
+
+		private static int _pointsDecimal;
+		private static int _signalDecimal;
+		private static int _berthsDecimal;
+
+
+		public static void Parse()
+		{
+			Console.WriteLine("Now Reading " + Settings.Default.saveGameDirectory);
+
+			try
+			{
+				string itemId;
+
+				_pointsDecimal = 0;
+				_signalDecimal = 0;
+				_berthsDecimal = 0;
+
+
+
+				StreamReader xmlData = new StreamReader(Settings.Default.saveGameDirectory);
+
+				while ((itemId = xmlData.ReadLine()) != null)
+				{
+			//		if (itemId.Contains("TBER ID=")) Berths_Parse(itemId);  //Berths
+					if (itemId.Contains("TPTS ID=")) Points_Parse(itemId);  //Points
+			//		if (itemId.Contains("TSIG ID=")) Signal_Parse(itemId);  //Signals
+				}
+
+
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("Parsing of file has failed. Does the save XML excist, or has it been moved?");
+				Console.WriteLine(e);
+			}
+
+		}
+
+
+
+		private static void Points_Parse(string itemId)
+		{
+			string pointsHex = _pointsDecimal.ToString("X").PadLeft(4, '0');    //Convert decimal counter to hex string
+
+			/*Want to take the specific item ID only, without the	*/
+			/*start and end of tag characters from the XML          */
+
+			{
+				itemId = itemId.TrimStart(' ');                   /*Want to take the spicific  */
+				itemId = itemId.Remove(0, 10);                    /*item ID only, wihtout the  */
+				itemId = itemId.TrimEnd('>');                     /*start and end of line chars*/
+				itemId = itemId.TrimEnd('"');
+			}
+			Console.WriteLine(itemId.PadRight(6, ' ') + "    " + pointsHex + "   " + _pointsDecimal);
+
+			_pointsDecimal++;
+		}
+
+		
+
+
+	}
+}
