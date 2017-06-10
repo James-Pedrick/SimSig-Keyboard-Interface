b4a4441293a8799332a3b00d7e66a422479b84d5
James Pedrick <james2.pedrick@outlook.com>
2017-06-10 22:40:22 +0100

    -

--- SimSig Keyboard Interface/User_Interface/Main_Menu.Designer.cs
+++ SimSig Keyboard Interface/User_Interface/Main_Menu.Designer.cs
@@ -653,7 +653,7 @@
 			// 
 			this.ttDisplay.BackColor = System.Drawing.SystemColors.WindowText;
 			this.ttDisplay.Dock = System.Windows.Forms.DockStyle.Bottom;
-			this.ttDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+			this.ttDisplay.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.ttDisplay.ForeColor = System.Drawing.Color.Cyan;
 			this.ttDisplay.FormattingEnabled = true;
 			this.ttDisplay.ItemHeight = 16;
